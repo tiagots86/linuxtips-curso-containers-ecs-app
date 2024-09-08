4af29658443d274a4707b45e0671da66eaf58992
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-09-08 10:16:46 -0300

    primeira versão

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+module "service" {
+  source = ".//c/Users/tiago/documents/github/linuxtips-curso-containers-ecs-service-module"
+
+  cluster_name     = var.cluster_name
+  service_name     = var.service_name
+  service_port     = var.service_port
+  service_cpu      = var.service_cpu
+  service_memory   = var.service_memory
+  service_listener = data.aws_ssm_parameter.service_listener.value
+
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  private_subnets = [
+    data.aws_ssm_parameter.private_subnet_1.value,
+    data.aws_ssm_parameter.private_subnet_2.value,
+    data.aws_ssm_parameter.private_subnet_3.value
+  ]
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,47 @@
+variable "region" {
+
+}
+
+variable "service_name" {
+
+}
+
+variable "cluster_name" {
+
+}
+
+variable "service_port" {
+
+}
+
+variable "service_cpu" {
+
+}
+
+variable "service_memory" {
+
+}
+
+variable "service_listener" {
+
+}
+
+variable "ssm_private_subnet_1" {
+
+}
+
+variable "ssm_private_subnet_2" {
+
+}
+
+variable "ssm_private_subnet_3" {
+
+}
+
+variable "ssm_lister" {
+
+}
+
+variable "ssm_vpc_id" {
+
+}
